7dc7bbc9797ac289b5bf339f1ebbd56b1fe2cf5d
Mustafa <mustafa.elbehery@meetrics.com>
2018-05-11 15:59:09 +0200

    Functor | Testes

--- src/main/scala/learnfp/functor/List.scala
+++ src/main/scala/learnfp/functor/List.scala
@@ -1,7 +1,10 @@
 package learnfp.functor
 
 object ListInstance {
-  implicit val listInstance:Functor[List] = new Functor[List] {
-    override def fmap[A, B](a: List[A])(fx: A => B): List[B] = ???
-  }
+    implicit val listInstance: Functor[List] = new Functor[List] {
+        override def fmap[A, B](a: List[A])(fx: A => B): List[B] = a match {
+            case Nil => Nil
+            case x :: xs => fx(x) :: fmap(xs)(fx)
+        }
+    }
 }
--- src/main/scala/learnfp/functor/Maybe.scala
+++ src/main/scala/learnfp/functor/Maybe.scala
@@ -1,25 +1,34 @@
 package learnfp.functor
 
 object Maybe {
-  sealed trait Maybe[+A]
-  case class Just[A](value: A) extends Maybe[A]
-  object Just {
-    def fmap[A, B](a: Just[A])(fx: A => B): Just[B] = Just[B](fx(a.value))
-  }
 
-  case class Nothing[A]() extends Maybe[A]
-  object Nothing {
-    def fmap[A, B](a: Nothing[A])(fx: A => B): Nothing[B] = Nothing[B]()
-  }
+    sealed trait Maybe[+A]
 
-  def nothing[A]():Maybe[A] = Nothing[A]()
-  def just[A](x:A):Maybe[A] = Just(x)
+    case class Just[A](value: A) extends Maybe[A]
+
+    object Just {
+        def fmap[A, B](a: Just[A])(fx: A => B): Just[B] = Just[B](fx(a.value))
+    }
+
+    case class Nothing[A]() extends Maybe[A]
+
+    object Nothing {
+        def fmap[A, B](a: Nothing[A])(fx: A => B): Nothing[B] = Nothing[B]()
+    }
+
+    def nothing[A](): Maybe[A] = Nothing[A]()
+
+    def just[A](x: A): Maybe[A] = Just(x)
 }
 
 object MaybeInstance {
-  import Maybe._
 
-  implicit val maybeInstance:Functor[Maybe] = new Functor[Maybe] {
-    override def fmap[A, B](a: Maybe[A])(fx: A => B): Maybe[B] = ???
-  }
+    import Maybe._
+
+    implicit val maybeInstance: Functor[Maybe] = new Functor[Maybe] {
+        override def fmap[A, B](a: Maybe[A])(fx: A => B): Maybe[B] = a match {
+            case Just(a) => Just[B](fx(a))
+            case _ => Nothing[B]()
+        }
+    }
 }
